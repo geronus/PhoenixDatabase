edae1ef7dceeae6cc5e6aae814875ff8cc84e1cb
mrcaboom <41026034+mrcaboom@users.noreply.github.com>
2018-09-03 23:36:13 -0400

    Update guildsave-auto.php
    Load lib function then connect

--- website/guildsave-auto.php
+++ website/guildsave-auto.php
@@ -1,8 +1,8 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
-include("connection.php");
 include_once('core-lib.inc.php');
+include("connection.php");
 
 if (isset($_POST['Members']) && is_array($_POST['Members'])) {
 	response_json(array('error'=>'This method of updating the DB is no longer active.'));
