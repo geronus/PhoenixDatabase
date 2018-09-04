cfc206f5a264b03646480c258addda3aeee8d3bd
mrcaboom <41026034+mrcaboom@users.noreply.github.com>
2018-09-03 23:37:38 -0400

    Update ranks-save-test.php

--- website/ranks-save-test.php
+++ website/ranks-save-test.php
@@ -1,8 +1,9 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
-include("connection.php");
+
 include_once('core-lib.inc.php');
+include("connection.php");
 
 foreach ($_POST['ranks'] AS $member) {
 	mysqli_query($link, "
